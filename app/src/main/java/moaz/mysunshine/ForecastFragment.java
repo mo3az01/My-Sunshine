4fe4a31cc0430f365c127b6e328669a9e0de8c7c
mo3az01 <mo3az014@gmail.com>
2016-06-16 14:18:04 +0200

    Lesson 3 - add location & unit setting , get real data when start the app

--- app/src/main/java/moaz/mysunshine/ForecastFragment.java
+++ app/src/main/java/moaz/mysunshine/ForecastFragment.java
@@ -1,10 +1,12 @@
 package moaz.mysunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -31,7 +33,6 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by XKCL0301 on 6/8/2016.
@@ -48,9 +49,9 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        String[] forcastArr = {"Today-Sunny-88/63", "Tomorrow-Foggy-70/46", "Weds-Cloudy-72/63", "Thurs-Rainy-64/51", "Fri-Foggy-70/46", "Sat-Sunny-76/68"};
-        List<String> forcastEntries = new ArrayList<>(Arrays.asList(forcastArr));
-        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, R.id.list_item_forecast_textview, forcastEntries);
+//        String[] forcastArr = {"Today-Sunny-88/63", "Tomorrow-Foggy-70/46", "Weds-Cloudy-72/63", "Thurs-Rainy-64/51", "Fri-Foggy-70/46", "Sat-Sunny-76/68"};
+//        List<String> forcastEntries = new ArrayList<>(Arrays.asList(forcastArr));
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item, R.id.list_item_forecast_textview, new ArrayList<String>());
 
         ListView list = (ListView) rootView.findViewById(R.id.listview_forecast);
         list.setAdapter(adapter);
@@ -60,8 +61,8 @@ public class ForecastFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 //                Toast.makeText(getActivity(),adapter.getItem(i),Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(getActivity(),DetialActivity.class)
-                        .putExtra(Intent.EXTRA_TEXT,adapter.getItem(i));
+                Intent intent = new Intent(getActivity(), DetialActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, adapter.getItem(i));
                 startActivity(intent);
             }
         });
@@ -78,11 +79,26 @@ public class ForecastFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                new FetchWeatherTask().execute("94043");
+                updateWeatherData();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeatherData();
+    }
+
+    private void updateWeatherData() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = sharedPreferences.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        String unit = sharedPreferences.getString(getString(R.string.pref_unit_key),
+                getString(R.string.pref_unit_default));
+        new FetchWeatherTask().execute(location,unit);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
@@ -100,7 +116,9 @@ public class ForecastFragment extends Fragment {
             String OPEN_WEATHER_MAP_API_KEY = "b948c32b38fe0eddc37e73e394d64503";
             String postalCode = params[0];
             String mode = "json";
-            String units = "metric";
+
+
+            String units = params[1];
             String cnt = "7";
 
             try {
