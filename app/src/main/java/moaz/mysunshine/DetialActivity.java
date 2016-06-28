c45c6c7bcd19ea9e28b6abf590e6680826a1d8ee
mo3az01 <mo3az014@gmail.com>
2016-06-28 10:26:58 +0200

    Lesson 4A - after add all test cases,, about location and weather tables

--- app/src/main/java/moaz/mysunshine/DetialActivity.java
+++ app/src/main/java/moaz/mysunshine/DetialActivity.java
@@ -3,9 +3,13 @@ package moaz.mysunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +18,15 @@ import android.widget.TextView;
 
 public class DetialActivity extends ActionBarActivity {
 
+    private ShareActionProvider mShareActionProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detial);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -52,9 +58,13 @@ public class DetialActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public static final String TAG = DetailFragment.class.getSimpleName();
+        public static final String AppName = "#SunshineApp";
+        private String mForecast;
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -63,10 +73,33 @@ public class DetialActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_detial, container, false);
             TextView tvForecast = (TextView) rootView.findViewById(R.id.tv_forecast);
             Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
-                tvForecast.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+                tvForecast.setText(mForecast);
+            }
 
             return rootView;
+        }
+
+        public Intent createShareForecastIntent(){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mForecast+AppName);
+            return shareIntent;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.menu_detailfragment,menu);
+            MenuItem item = menu.findItem(R.id.menu_item_share);
+            ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if(mShareActionProvider != null){
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }else{
+                Log.d("details fragment","ActionProvider is null");
+            }
+
         }
     }
 }
